9d158059ad6c8a803fcdf9b7a5653fdc5703f3b4
Craig Berry <hi@craigberry.co>
2020-10-26 17:05:56 +1100

    Update test structure for Laravel 8
    - delete empty tests
    - update factory structure

--- src/Block/BlockModel.php
+++ src/Block/BlockModel.php
@@ -2,8 +2,9 @@
 
 use Anomaly\HtmlBlockExtension\Block\Contract\BlockInterface;
 use Anomaly\Streams\Platform\Model\HtmlBlock\HtmlBlockBlocksEntryModel;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class BlockModel extends HtmlBlockBlocksEntryModel implements BlockInterface
 {
-
+    use HasFactory;
 }
